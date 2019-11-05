312dc7103322dceb8d912362e6758633910f6031
sune <sune1309@edu.easj.dk>
2019-11-05 14:29:52 +0100

    hej

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,10 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-
+import java.io.FileInputStream;
 
 
 public class Main extends Application {
@@ -19,9 +21,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hej");
 
+        FileInputStream input = new FileInputStream("/home/sune/Desktop/hejr.png");
+        Image image  = new Image(input);
+        ImageView imageView = new ImageView(image);
+
         TextField textField = new TextField();
 
-        Button button = new Button("click to get text");
+        Button button = new Button("meme" , imageView);
+
+        button.setWrapText(true);
 
         button.setOnAction(action -> {
             System.out.println(textField.getText());
@@ -29,7 +37,7 @@ public class Main extends Application {
         HBox box = new HBox(textField, button);
         box.getChildren().addAll();
 
-        Scene scene = new Scene(box, 400 , 200);
+        Scene scene = new Scene(box, 1000 , 1000);
         primaryStage.setScene(scene);
         primaryStage.show();
 
